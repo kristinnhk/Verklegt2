476da74a42ababf89de6cfa6e765f15c1cc6f283
Kristinn <Kristinnhk@hotmail.com>
2015-05-05 11:55:02 +0000

    fixing git authentication testing

--- StokerSolution/Stoker/Controllers/HomeController.cs
+++ StokerSolution/Stoker/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-//erase thsi test text <3
+
 namespace Stoker.Controllers
 {
     public class HomeController : Controller
