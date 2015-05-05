434807cf0ed43c6f609096bd8ed7580cab0c28b0
Kristinn <Kristinnhk@hotmail.com>
2015-05-05 00:29:38 +0000

    first commit

---
+++ StokerSolution/Stoker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stoker.Startup))]
+namespace Stoker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
