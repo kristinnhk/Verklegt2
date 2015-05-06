38a4d276b7c95092c59c96157959c869392ed137
Kristinn <Kristinnhk@hotmail.com>
2015-05-06 18:19:06 +0000

    login fix

--- StokerSolution/Stoker/Controllers/AccountController.cs
+++ StokerSolution/Stoker/Controllers/AccountController.cs
@@ -90,7 +90,7 @@ namespace Stoker.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser() { UserName = (model.firstName + model.lastName), Email = model.Email,
+                var user = new ApplicationUser() { UserName = model.Email, Email = model.Email,
                 firstName = model.firstName, lastName = model.lastName};
                 IdentityResult result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
