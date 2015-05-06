1098d1edaa875f1ccd0355adb8fc4e995072618b
Kristinn <Kristinnhk@hotmail.com>
2015-05-06 12:27:56 +0000

    viewmodel added

---
+++ StokerSolution/Stoker/Models/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stoker.Models
+{
+    /// <summary>
+    /// A class to carry information to the views
+    /// 
+    /// </summary>
+    public class ViewModel
+    {
+        // list of user model ??
+        public List<GroupModel> groups { get; set; }
+        public List<InterestModel> interests { get; set; }
+        public List<ThreadModel> threads { get; set; }
+        public List<CommentModel> comments { get; set; }
+
+    }
+}
