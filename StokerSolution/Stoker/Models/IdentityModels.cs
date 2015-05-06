288d246743a61f525c2b2316b58d753c15bebc2c
Kristinn <Kristinnhk@hotmail.com>
2015-05-06 16:43:58 +0000

    getting in sync

--- StokerSolution/Stoker/Models/IdentityModels.cs
+++ StokerSolution/Stoker/Models/IdentityModels.cs
@@ -2,14 +2,17 @@
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace Stoker.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+       // public string Firstname { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
+
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
@@ -19,6 +22,11 @@ namespace Stoker.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        /*public DbSet<CommentModel> comments { get; set; }
+        public DbSet<GroupModel> groups { get; set; }
+        public DbSet<InterestModel> interests { get; set; }
+        public DbSet<ThreadModel> threads { get; set; }*/
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
