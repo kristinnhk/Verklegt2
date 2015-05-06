97010677d7eb36217d4fc503ccc995bd7ad396ce
Skúli Þór Árnason <skuliarnas@gmail.com>
2015-05-06 14:28:59 +0000

    Testing views, nothing works

--- StokerSolution/Stoker/Controllers/HomeController.cs
+++ StokerSolution/Stoker/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Stoker.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("FrontPageView");
         }
 
         public ActionResult About()
