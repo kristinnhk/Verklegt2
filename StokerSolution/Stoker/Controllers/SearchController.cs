9349feff77ec6c86997056793fc62c21764cc220
Skúli Þór Árnason <skuliarnas@gmail.com>
2015-05-09 17:26:15 +0000

    Search virkar, hardcoded

---
+++ StokerSolution/Stoker/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using Stoker.Models;
+using Stoker.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stoker.Controllers
+{
+    public class SearchController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Search
+        public ActionResult Search(string query)
+        {
+            UserService userService = new UserService();
+            ThreadService threadService = new ThreadService();
+            InterestService interestService = new InterestService();
+            GroupService groupService = new GroupService();
+
+            ViewModel results = new ViewModel();
+
+            List<ApplicationUser> users = userService.GetUsersByName("S").ToList();
+            List<GroupModel> groups = groupService.GetGroupByTitle("Steinn").ToList();
+            //List<InterestModel> interests = interestService.GetInterestsByTitle(query).ToList();
+            //List<ThreadModel> threads = threadService.GetThreadByTitle(query).ToList();
+            //results.interests = interests;
+            //results.threads = threads;
+            results.groups = groups;
+            results.Users = users;
+
+            return View(results);
+        }
+    }
+}
