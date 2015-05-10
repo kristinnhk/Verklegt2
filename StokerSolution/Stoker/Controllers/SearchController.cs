226428e07eac9a4db1e72261b776cb76a43d5b90
Skúli Þór Árnason <skuliarnas@gmail.com>
2015-05-10 21:32:31 +0000

    commented code, removed link to removed js file

--- StokerSolution/Stoker/Controllers/SearchController.cs
+++ StokerSolution/Stoker/Controllers/SearchController.cs
@@ -21,7 +21,13 @@ namespace Stoker.Controllers
         {
             return View();
         }
-
+        /// <summary>
+        /// This function uses a ajax script in the SearchJS.js file to search for all
+        /// objects in the database with a recieved value. It does this by taking the value
+        /// from a textfield, sending it to this function through an ajax post method and
+        /// then returning a json object that appends the values to a table in Search.cshtml.
+        /// </summary>
+        /// <returns>Function returns a json object that includes everything thatsatisfies the query</returns>
         public ActionResult SearchJson()
         {
             ViewModel results = new ViewModel();
