889431ed0a323b7feb1b6cc472864f6388f107eb
steinnp <steinnp14@ru.is>
2015-05-12 15:41:14 +0000

    GroupSettings view

---
+++ StokerSolution/Stoker/Controllers/GroupSettingsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Stoker.Models;
+using Stoker.Services;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace Stoker.Controllers
+{
+    public class GroupSettingsController : Controller
+    {
+        
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private GroupService groupService = new GroupService();
+
+        public ActionResult GroupSettings(int groupID)
+        {
+            ViewModel model = new ViewModel();
+
+            GroupModel group = groupService.GetGroupByID(groupID);
+
+            model.groups = new List<GroupModel>();
+            model.groups.Add(group);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public void UpdateAboutGroup()
+        {
+            string aboutGroupString = Request["aboutGroup"].ToString();
+            int groupID = Convert.ToInt32(Request["groupID"]);
+           groupService.SetGroupAbout(groupID, aboutGroupString);
+        }
+	}
+}
---
+++ StokerSolution/Stoker/Scripts/GroupSettingScripts.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    //Sends an ajax post request to the controller to update the about me section of the users page.
+    $('#groupIDText').hide();
+    if (!$('textarea#AboutTextarea').val().trim()) { //if the about section is empty
+        $('#AboutGroupError').text("About me can not be empty");
+    }
+    else {
+        $('#AboutGroupError').text("");
+    }
+
+    $('#AboutSubmit').click(function () {
+        if (!$('textarea#AboutTextarea').val().trim()) { //if the about section is empty
+            $('#AboutGroupError').text("About me can not be empty");
+        }
+        else {
+            $('#AboutGroupError').text("");
+            var returnString = {
+                'aboutGroup': $('textarea#AboutTextarea').val().trim(), 'groupID': $('#groupIDText').text()
+            };
+            var posting = $.post('/GroupSettings/UpdateAboutGroup/', returnString);
+
+        }
+    });
+});
