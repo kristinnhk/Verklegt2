85e9f25b26167c6e91812e73e1f5151332bb44e1
steinnp <steinnp14@ru.is>
2015-05-12 20:25:12 +0000

    settings view klarud

--- StokerSolution/Stoker/Scripts/AddGroupScripts.js
+++ StokerSolution/Stoker/Scripts/AddGroupScripts.js
@@ -1,17 +1,21 @@
 ﻿$(document).ready(function () {
     document.getElementById("submitGroup").disabled = true;
+    $("#submitGroup").val("Title or About me can not be empty");
 
     function isValid() {
         if ($('#about').val().trim() != '') {
             if ($('#title').val().trim() != '') {
                 document.getElementById("submitGroup").disabled = false;
+                $("#submitGroup").val("Create group");
             }
             else {
                 document.getElementById("submitGroup").disabled = true;
+                $("#submitGroup").val("Title or About me can not be empty");
             }
         }
         else {
             document.getElementById("submitGroup").disabled = true;
+            $("#submitGroup").val("Title or About me can not be empty");
         }
     };
     $('#about').keyup(isValid);
--- StokerSolution/Stoker/Scripts/GroupSettingScripts.js
+++ StokerSolution/Stoker/Scripts/GroupSettingScripts.js
@@ -1,12 +1,25 @@
 ﻿$(document).ready(function () {
     //Sends an ajax post request to the controller to update the about me section of the users page.
     $('#groupIDText').hide();
-    if (!$('textarea#AboutTextarea').val().trim()) { //if the about section is empty
-        $('#AboutGroupError').text("About me can not be empty");
+    if ($('#AboutTextarea').val().trim() == '') { //if the about section is empty
+        document.getElementById("AboutSubmit").disabled = true;
+        $("#AboutSubmit").text("About me section can not be empty");
     }
-    else {
-        $('#AboutGroupError').text("");
-    }
+
+    function isValid() {
+        if ($('#AboutTextarea').val().trim() != '') {
+            document.getElementById("AboutSubmit").disabled = false;
+            $("#AboutSubmit").text("Change about group");
+            
+        }
+        else {
+            document.getElementById("AboutSubmit").disabled = true;
+            $("#AboutSubmit").text("About me section can not be empty");
+        }
+    };
+    $('#AboutTextarea').keyup(isValid);
+
+
 
     $('#AboutSubmit').click(function () {
         if (!$('textarea#AboutTextarea').val().trim()) { //if the about section is empty
