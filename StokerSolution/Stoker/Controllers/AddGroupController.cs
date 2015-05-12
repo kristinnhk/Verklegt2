a2d591db759d904d3a8d80d74b588abb05f44d2a
steinnp <steinnp14@ru.is>
2015-05-12 17:24:07 +0000

    Add group og group settings virkar

---
+++ StokerSolution/Stoker/Controllers/AddGroupController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Stoker.Models;
+using Stoker.Services;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace Stoker.Controllers
+{
+    public class AddGroupController : Controller
+    {
+        GroupService service = new GroupService();
+        //
+        // GET: /AddGroup/
+        public ActionResult AddGroup()
+        {
+            return View();
+        }
+
+        public ActionResult Add(GroupModel model)
+        {
+            model.numberOfGroupMembers = 1;
+            service.SetGroup(model);
+            return RedirectToAction("GroupProfile", "GroupProfile", new { groupID =  model.groupID});
+        }
+	}
+}
---
+++ StokerSolution/Stoker/Scripts/AddGroupScripts.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    document.getElementById("submitGroup").disabled = true;
+
+    function isValid() {
+        if ($('#about').val().trim() != '') {
+            if ($('#title').val().trim() != '') {
+                document.getElementById("submitGroup").disabled = false;
+            }
+            else {
+                document.getElementById("submitGroup").disabled = true;
+            }
+        }
+        else {
+            document.getElementById("submitGroup").disabled = true;
+        }
+    };
+    //$('#about').keydown(isValid());
+    $('#about').keyup(isValid);
+    $('#title').keyup(isValid);
+});
