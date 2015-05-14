9eac08568fdc5f0eee5e441315ac83957eae7de5
steinnp <steinnp14@ru.is>
2015-05-14 17:48:35 +0000

    vista

--- StokerSolution/Stoker/Scripts/Likes.js
+++ StokerSolution/Stoker/Scripts/Likes.js
@@ -1,20 +1,53 @@
 ﻿$(document).ready(function () {
-    var likeString = { 'threadID': $('#threadID').val() };
-    var isFollowing = $.post('/Stoker/IsLikedThread/', likeString);
-    var following = isFollowing.done(function (result) {
-        if (result == "True") {
+
+    var likes = [];
+
+   $(".likeButton").each(function ()
+    {
+        var value = $(this).val();
+        likes.push(value);
+   });
+
+   var likesString = likes.map(function (obj) {
+       return obj.toString();
+   });
+    for(var i = 0; i < likes.length; i++)
+    {
+        var postString = { "threadID": likes[i] };
+        var isLiked = $.post('/Stoker/IsLikedThread/', postString);
+        isLiked.done(function (result)
+        {
+            
+            
+            if (result != "True")
+            {
+                var likeString = ".likeButton[value='" + likesString[i] + "'";
+                alert(likeString);
+                //$(".likeButton[value=")
+            }
+        });
+    };
+
+   /* var isFollowing = $.post('/Stoker/IsLikedThread/', likeString);
+    var following = isFollowing.done(function (result) 
+    {
+        if (result == "True") 
+        {
             $('#likebutton').attr('value', 'Unlike');
         }
     });
-    $("#likebutton").click(function () {
-        if ($(this).val() == "Like") {
+    $("#likebutton").click(function () 
+    {
+        if ($(this).val() == "Like") 
+        {
             $(this).attr('value', 'Unlike');
             var returnString = { 'threadID': $('#threadID').val() };
             var posting = $.post('/Stoker/LikeThread/', returnString);
-        } else {
+        } else 
+        {
             $(this).attr('value', 'Follow');
             var returnString = { 'threadID': $('#threadID').val() };
             var posting = $.post('/Stoker/UnLikeThread/', returnString);
         }
-    })
+    });*/
 });
