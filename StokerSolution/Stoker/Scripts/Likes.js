53a93fe2313156fb0fe1ccf429937b452d588d05
Skúli Þór Árnason <skuliarnas@gmail.com>
2015-05-14 16:10:01 +0000

    fullt af java

---
+++ StokerSolution/Stoker/Scripts/Likes.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    var likeString = { 'threadID': $('#threadID').text() };
+    var isFollowing = $.post('/Stoker/IsLikedThread/', likeString);
+    var following = isFollowing.done(function (result) {
+        if (result == "True") {
+            $('#likebutton').attr('value', 'Unlike');
+        }
+    });
+    $("#likebutton").click(function () {
+        if ($(this).val() == "Like") {
+            $(this).attr('value', 'Unlike');
+            var returnString = { 'id': $('#threadID').val() };
+            var posting = $.post('/Stoker/LikeThread/', returnString);
+        } else {
+            $(this).attr('value', 'Follow');
+            var returnString = { 'id': $('#threadID').val() };
+            var posting = $.post('/Stoker/UnLikeThread/', returnString);
+        }
+    })
+});
---
+++ StokerSolution/Stoker/Scripts/LikesComment.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    var likeString = { 'commentID': $('#commentID').text() };
+    var isFollowing = $.post('/Stoker/IsLikedComment/', likeString);
+    var following = isFollowing.done(function (result) {
+        if (result == "True") {
+            $('#likebutton').attr('value', 'Unlike');
+        }
+    });
+    $("#likebutton").click(function () {
+        if ($(this).val() == "Like") {
+            $(this).attr('value', 'Unlike');
+            var returnString = { 'id': $('#commentID').val() };
+            var posting = $.post('/Stoker/LikeComment/', returnString);
+        } else {
+            $(this).attr('value', 'Follow');
+            var returnString = { 'id': $('#commentID').val() };
+            var posting = $.post('/Stoker/UnLikeComment/', returnString);
+        }
+    })
+});
